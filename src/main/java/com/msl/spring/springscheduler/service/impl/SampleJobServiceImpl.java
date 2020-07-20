71d9acfeab1ded051a59e12de7c51e011fed9b2c
msalas <msalas@vectoritcgroup.com>
2020-07-20 18:53:01 +0200

    API Rest for conjob dynamic scheduling

--- src/main/java/com/msl/spring/springscheduler/service/impl/SampleJobServiceImpl.java
+++ src/main/java/com/msl/spring/springscheduler/service/impl/SampleJobServiceImpl.java
@@ -1,5 +1,7 @@
 package com.msl.spring.springscheduler.service.impl;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.stereotype.Service;
@@ -12,24 +14,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class SampleJobServiceImpl implements SampleJobService{
 
-	public static final long EXECUTION_TIME = 5000L;
+	public static final long EXECUTION_TIME = 1000;
 
 	private AtomicInteger count = new AtomicInteger();
 
 	public void executeSampleJob() {
 
-		LOGGER.warn("The sample job has begun...");
+		LOGGER.warn("The sample job has begun... count:" + count + " in host:" + getHostAndIp() );
 		try {
 			Thread.sleep(EXECUTION_TIME);
 		} catch (InterruptedException e) {
 			LOGGER.error("Error while executing sample job", e);
 		} finally {
 			count.incrementAndGet();
-			LOGGER.info("Sample job has finished...");
+			LOGGER.warn("Sample job has finished...");
 		}
 	}
 
 	public int getNumberOfInvocations() {
 		return count.get();
 	}
+	
+    private static String getHostAndIp() {
+    	 
+        InetAddress ip = null;
+        String hostname = "";        
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName(); 
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return hostname + "/" + ip;
+    }
 }
